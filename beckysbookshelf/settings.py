c9c370dce95074c5259bd38e531ea378ae47b45e
haydenhoopes <hayden.hoopes@aggiemail.usu.edu>
2021-05-19 22:17:26 -0600

    added some config stuff for deployment

--- beckysbookshelf/settings.py
+++ beckysbookshelf/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATICFILES_LOCATION = 'static'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'beckysbookshelf/static')
 ]
