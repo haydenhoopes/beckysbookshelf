6cc6dc4e30287a0d5c793394e99ed3a2892fcdff
haydenhoopes <hayden.hoopes@aggiemail.usu.edu>
2021-05-19 22:40:21 -0600

    added some config stuff for deployment

--- beckysbookshelf/settings.py
+++ beckysbookshelf/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -160,3 +161,4 @@ EMAIL_HOST_PASSWORD = 'P@ssw0rdReset'
 # import dj_database_url
 # db_from_env = dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(db_from_env)
+django_heroku.settings(locals())
